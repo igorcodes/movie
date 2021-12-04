5714ba86a445c44aa62f8467bbf7b4bea5fbc606
IgorZakutajlo <87570302+igorcodes@users.noreply.github.com>
2021-12-04 13:17:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,22 +13,22 @@ import { BrowserRouter, Route } from "react-router-dom";
 const cookies = new Cookies();
 
 export default class App extends React.Component {
-  constructor() {                              //создаю состояние конструктор, фильтров вверху всего кода (стандартно) для того чтобы при смене селекта-фильта мы отправляли новый запрос для получения нового VideoList. Детектит изменение пропсов и делает новый запрос  
+  constructor() {                               
     super()
 
-    this.state = {                              //все вильтры и их значения буду записывать в обьект filters
+    this.state = {                              
       user: null,
       session_id: null,
     };
   }
 
-  updateUser = user => {                        //нужно передать эту функцию в LoginForm, для этого передаю в Header (updateUser={this.updateUser}), после перехожу в Header (он уже в пропсах Header), и в хедере нахожу компоненту Login и передаю туда ( <Login updateUser={this.props.updateUser} /> ), перехожу в Login и уже там в LoginForm передаю пропс ( <LoginForm updateUser={this.props.updateUser} /> ) и уже после этого я могу вызывать его в LoginForm 
+  updateUser = user => {                        
     this.setState({
       user
     });
   };
   
-  updateSessinId = session_id => {        //нужно передать эту функцию в LoginForm, для этого передаю в Header (updateUser={this.updateUser}), после перехожу в Header (он уже в пропсах Header), и в хедере нахожу компоненту Login и передаю туда ( <Login updateUser={this.props.updateUser} /> ), перехожу в Login и уже там в LoginForm передаю пропс ( <LoginForm updateUser={this.props.updateUser} /> ) и уже после этого я могу вызывать его в LoginForm 
+  updateSessinId = session_id => {        
     cookies.set("session_id", session_id, {
       path: "/",
       maxAge: 2592000
@@ -39,7 +39,7 @@ export default class App extends React.Component {
   };
 
   logOut = () => {
-    cookies.remove("session_id")                     //при logOut удаляю куки по session_id, обнуляю session_id, обнуляю user
+    cookies.remove("session_id")                     
     this.setState({
       session_id: null,
       user: null
@@ -47,13 +47,13 @@ export default class App extends React.Component {
   };
 
   
-                                                                //в дидмаунте считаваю сессион айди с куки, если она пустая запрос не выполняем, если там есть сессион ай ди, мы вызываем запрос, получаем юзера, (без модального окна) сразу будем видеть аватар 
-  componentDidMount() {                                  //при первом рендере делаем запрос компонент дидмаунт
-    const session_id = cookies.get("session_id");         //делать запрос callApi только если уже есть session_id
+                                                                
+  componentDidMount() {                                  
+    const session_id = cookies.get("session_id");         
     console.log(session_id);
-    if (session_id) {                                  //если session_id не пустое (если мы логинились раньше то оно не пустое) то я вызываю callApi
+    if (session_id) {                                  
       callApi(`${API_URL}/account?api_key=${API_KEY_3}&session_id=${session_id}`)
-      .then(user => {                                  //после этого в then делаю апдейт юзера
+      .then(user => {                                  
         this.updateUser(user);
         this.updateSessinId(session_id);
       });
@@ -61,11 +61,11 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { user, session_id} = this.state;         //без этого ниже <Filters page={this.state.page} filters={this.state.filters}
+    const { user, session_id} = this.state;         
     return (
       <BrowserRouter>          
 
-          <Header user={user} updateUser={this.updateUser} session_id={session_id} logOut={this.logOut} updateSessinId={this.updateSessinId} />    {/* 1 в хедер его передали */}
+          <Header user={user} updateUser={this.updateUser} session_id={session_id} logOut={this.logOut} updateSessinId={this.updateSessinId} />    
           {/* <Link to="/">Go to home</Link><br/>
           <Link to="/my-library">Избранные</Link><br/>    */}       
 
@@ -81,6 +81,6 @@ export default class App extends React.Component {
 
 
 
-/* ,:item.title */
+
 
 
